570ca1abfa5eb490456829fe78178a50bf1d48b7
shuja <shuja.gmail.com>
2023-06-20 10:47:50 +0530

    account id

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,9 @@
+data "aws_caller_identity" "current" {}
+
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
+  name_regex  = "devops-practice-with-ansible"
+  owners      = [data.aws_caller_identity.current.account_id]
 }
   resource "aws_instance" "ec2" {
   ami           = data.aws_ami.ami.image_id
