9af47a52c8b6cf17cef12f061fa3b612dd95fb1b
shuja <shuja.gmail.com>
2023-06-28 18:31:35 +0530

    state.tfvars

--- aws parameters/main.tf
+++ aws parameters/main.tf
@@ -1,7 +1,1 @@
-resource "aws_ssm_parameter" "parameter" {
-  count = length(var.parameters)
-  name  = var.parameters[count.index].name
-  type  = var.parameters[count.index].type
-  value = var.parameters[count.index].value
-}
-variable "parameters" {}
+
